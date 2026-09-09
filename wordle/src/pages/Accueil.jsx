b0fb642f52fe57d069ecde3aa9fe314edf33a6da
dewen <ewen.denoual@ynov.com>
2026-09-09 14:46:35 +0200

    add working version, will work on pages

---
+++ wordle/src/pages/Accueil.jsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import "./Accueil.css";
+
+function Accueil() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="accueil">
+      <div className="contenu">
+        <h1>Bienvenue sur mon site</h1>
+
+        <p>
+          Découvrez mon application React.
+        </p>
+
+        <button 
+          onClick={() => navigate("/suivant")}
+        >
+          Commencer
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Accueil;
---
+++ wordle/src/pages/PageSuivante.jsx
@@ -0,0 +1,9 @@
+function PageSuivante() {
+  return (
+    <div>
+      <h1>Bienvenue sur la page suivante !</h1>
+    </div>
+  );
+}
+
+export default PageSuivante;
